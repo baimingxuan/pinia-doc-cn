b72595c35c5ae1b32cb7a236cb8116ead0697ec9
baimingxuan <354372738@qq.com>
2022-02-21 11:04:14 +0800

    配置文件优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,29 +1,54 @@
 module.exports = {
-    title: 'Pinia中文文档',
-    description: 'Pinia中文文档(详细翻译官方文档)',
+    title: 'Pinia 中文文档',
+    description: 'Pinia 中文文档(详细翻译官方文档)',
     themeConfig: {
         nav: [
-            { text: '指南', link: '' },
-            { text: 'GitHub', link: 'https://github.com/baimingxuan/pinia-doc-cn' },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/baimingxuan/pinia-doc-cn'
+            }
         ],
         sidebar: [
             {
                 title: '介绍',
                 path: '/introduction/Introduction',
+                collapsable: false,
                 children: [
-                    { title: 'Pinia是什么？', path: '/introduction/Introduction' },
-                    { title: '快速上手', path: '/introduction/Installation' }
+                    {
+                        title: 'Pinia是什么？',
+                        path: '/introduction/Introduction'
+                    },
+                    {
+                        title: '快速上手',
+                        path: '/introduction/Installation'
+                    }
                 ]
             },
             {
                 title: '核心概念',
                 path: '/core/defined',
+                collapsable: false,
                 children: [
-                    { title: '定义Store', path: '/core/defined' },
-                    { title: 'State', path: '/core/State' },
-                    { title: 'Getters', path: '/core/Getters' },
-                    { title: 'Actions', path: '/core/Actions' },
-                    { title: 'Plugins', path: '/core/Plugins' }
+                    {
+                        title: '定义Store',
+                        path: '/core/defined'
+                    },
+                    {
+                        title: 'State',
+                        path: '/core/State'
+                    },
+                    {
+                        title: 'Getters',
+                        path: '/core/Getters'
+                    },
+                    {
+                        title: 'Actions',
+                        path: '/core/Actions'
+                    },
+                    {
+                        title: 'Plugins',
+                        path: '/core/Plugins'
+                    }
                 ]
             }
         ]
