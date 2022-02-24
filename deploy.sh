a84e9a1ed6252f2bab1415a37fdd4a548939023a
baimingxuan <354372738@qq.com>
2022-02-24 15:47:03 +0800

    添加 deploy.sh 脚本文件

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+#创建.nojekyll 防止Github Pages build错误
+touch .nojekyll
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到github仓库
+git push -f git@github.com:baimingxuan/pinia-doc-cn.git master:gh-pages
+
+cd -
