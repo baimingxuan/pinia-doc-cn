067b855c0c42fe99f76c735c8b815f16fdf6c862
baimingxuan <354372738@qq.com>
2022-03-01 15:19:53 +0800

    侧边栏配置config.js修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -75,6 +75,37 @@ module.exports = {
                         path: '/ssr/nuxt'
                     }
                 ]
+            },
+            {
+                title: '操作手册',
+                path: '/cookbook/migration-vuex',
+                collapsable: false,
+                children: [
+                    {
+                        title: '从Vuex≤4迁移',
+                        path: '/cookbook/migration-vuex'
+                    },
+                    {
+                        title: 'HMR(热模块更新)',
+                        path: '/cookbook/hot-module-replacement'
+                    },
+                    {
+                        title: 'Testing(测试)',
+                        path: '/cookbook/testing'
+                    },
+                    {
+                        title: '不使用setup()的用法',
+                        path: '/cookbook/options-api'
+                    },
+                    {
+                        title: '组合Stores',
+                        path: '/cookbook/composing-stores'
+                    },
+                    {
+                        title: '从0.x(v1)迁移到v2',
+                        path: '/cookbook/migration-v1-v2'
+                    }
+                ]
             }
         ],
         docsRepo: 'baimingxuan/pinia-doc-cn',
